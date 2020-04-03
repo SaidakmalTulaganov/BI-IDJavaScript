5f5a6af44273ec793c5c0b701f9969d9751a0334
SaidakmalTulaganov <62115986+SaidakmalTulaganov@users.noreply.github.com>
2020-04-03 18:26:18 +0300

    Add files via upload

--- zadanie2.js
+++ zadanie2.js
@@ -1,17 +1,53 @@
-let array = new Array();
-var arr = new Array();
-for(let i = 0; i < 8; i++) {
-  array[i] = prompt("array[" + i + "]", "");
-}
-for(var i = 0; i < 8; i++) {
-    arr[i] = new Array();
-    for(var j = 0; j < 8; j++){
-      arr[i][j] = Math.pow(array[j], i);
+let x = 5,    // позиция по х
+    y = 4,    // позиция по у
+    ka = 1.732, // коэффициент ОА
+    k = 1,    // коэфиициент прямой движения
+    dx = -0.1,    // шаг по оси х
+    dy = -0.1;    // шаг по оси у
+    while(true) {
+        if(y <= 0) { // столкновение с нижней гранью
+            if(x < 5) {
+                y = 0;
+                console.log('[ bottom bump ] x: ' + x + ' y: ' + y);
+            }
+            dy = - dy;
+            k  = -k;
+            console.log('[ directions ] dx ' + dx + ' dy ' + dy );
+        }
+        if(y >= ka * x) { // столкновение с боковой гранью
+            if(x < 5) {
+                console.log('[ side bump ] x: ' + x + ' y: ' + y);
+            }
+            let angle = newAngle(k, ka);
+                angle = 90 - angle;
+                console.log('[ new angle ] ' + angle);     
+                if(angle < 45) {
+                    dx = -dx;
+                    dy = -dx;
+                    if(dy < 0) {
+                        k = Math.tan((angle + 30) * Math.PI / 180);
+                    } else {
+                        k = Math.tan((30 - angle) * Math.PI / 180);
+                    }
+                    dy *= k;
+                } else {
+                    dx = -dx;
+                    dy = -dx;
+                    if(dy < 0) {
+                        k = Math.tan((angle + 30) * Math.PI / 180);
+                    } else {
+                        k = Math.tan((30 - angle) * Math.PI / 180);
+                    }
+                    dy *= k;
+                }
+                console.log('[ directions ] dx ' + dx + ' dy ' + dy );
+        }
+        if(x > 10 || y > 10 || x < 0 || y < 0) { // выход за пределы поля
+            break;
+        }
+        x += dx;
+        y += dy;
     }
-}
-for(let i = 0; i < 8; i++) {
-  for(let j = 0; j < 8; j++) {
-    console.log(arr[i][j]);
-  }
-  console.log(" ");
+    function newAngle(k1, k2) {
+    return Math.round(Math.atan(Math.abs((k1 - k2) / (k1 * k2 + 1))) * 180 / Math.PI);
 }
